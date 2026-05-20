10e0a1eba3d281288667b370b8c0712ec5eb5f2f
Zulnurain Arshad <meet.zulnurain@gmail.com>
2026-05-20 16:04:58 +0200

    feat:delete extra file

--- runserver.py
+++ runserver.py
@@ -1,14 +1,14 @@
 """
-This script runs the FlaskWebProject1 application using a development server.
+This script runs the Flask application using a development server.
 """
 
 from os import environ
-from FlaskWebProject1 import app
+from app import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(environ.get('SERVER_PORT', '8000'))
+        PORT = int(environ.get('SERVER_PORT', '5000'))
     except ValueError:
-        PORT = 5555
+        PORT = 5000
     app.run(host=HOST, port=PORT)
